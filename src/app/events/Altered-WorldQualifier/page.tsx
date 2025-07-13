6b09858a10ddb42b67b1efee27fe2741b6cabd6c
Siro <siro47@gmail.com>
2025-07-13 23:28:07 +0200

    Finished altered worlds page

--- src/app/events/Altered-WorldQualifier/page.tsx
+++ src/app/events/Altered-WorldQualifier/page.tsx
@@ -4,6 +4,7 @@ import { Metadata } from "next";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendar, faClock, faMap, faMoneyBill1} from "@fortawesome/free-regular-svg-icons";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
 export const metadata: Metadata = {
   title: "Altered - World Championship Qualifier Spain 2025 | SimonV TCG",
@@ -35,7 +36,7 @@ const SWUPage = () => {
         <div className="mt-12">
           <div className="pb-4">
             <FontAwesomeIcon icon={faCalendar} className="fas fa-calendar fa-2xl mr-8"></FontAwesomeIcon>
-            6 Septiembre 2025
+            6 y 7 de Septiembre de 2025
           </div>
           <div className="pb-4">
             <FontAwesomeIcon icon={faClock} className="fas fa-clock fa-2xl mr-8"></FontAwesomeIcon>
@@ -58,17 +59,18 @@ const SWUPage = () => {
           </div>
           <div className="mt-8">
             <div className='text-sm'>
-                <p className='pb-4'>Estamos encantados de albergar el Clasificatorio para el Campeonato Mundial de Altered TCG en Barcelona, España.</p>
+                <p className='pb-4'>Estamos encantados de colaborar con el Clasificatorio para el Campeonato Mundial de Altered TCG en Barcelona, España.</p>
                 <p className='pb-4'>Compite con otros Expedicionarios por la oportunidad de unirte a los campeones en el Campeonato Mundial de Altered que se celebrará en París a finales de este año. Este evento final tiene como objetivo ser una gran celebración comunitaria del primer aniversario de Altered, donde se coronará al primer Campeón Mundial.</p>
             </div>
             <h1 className='text-xl font-bold pb-8 mt-8'>Detalles del evento</h1>
             <div className='text-sm'>
                 <ul className='pb-4 list-disc'>
-                    <li className="p-2">Game format: Standard Constructed (Beyond the Gates, Trial by Frost & Whispers from the Maze)</li>
-                    <li className="p-2">Rules Enforcement Level: Competitive (Swiss rounds), High stakes (Top cut)</li>
-                    <li className="p-2">Rounds: Swiss, 30 minutes, Best-of-One</li>
-                    <li className="p-2">Total rounds: up to 7 (depending on attendance)</li>
-                    <li className="p-2">Top 8 Cut (Best-of-Three; single elimination; quarterfinals: 50 minutes time, semifinals/finals: untimed)</li>
+                    <li className="p-2">Formato de juego: Standard Construido (Beyond the Gates, Trial by Frost & Whispers from the Maze)</li>
+                    <li className="p-2">Nivel de cumplimiento de reglas: Competitivo (Rondas Suizo), High stakes (Top cut)</li>
+                    <li className="p-2">Rondas: Suizo, 30 minutos, Best-of-One</li>
+                    <li className="p-2">Rondas totales: Hasta 7 (dependiendo del nº de participantes)</li>
+                    <li className="p-2">El top 32 pasa a la segunda ronda que se celebra el domingo</li>
+                    <li className="p-2">Top 8 (Best-of-Three; eliminación simple; cuartos de final: 50 minutos, semifinales/final: sin tiempo)</li>
                 </ul>
             </div>
           </div>
@@ -76,15 +78,40 @@ const SWUPage = () => {
       </div>
 
       <div className="container grid grid-cols-1">
-        <div className="px-20">
-          <div className="relative aspect-[3/4] animate-fadeInLeft">
+        <div className="px-10">
+          <h1 className='text-xl font-bold pb-8 mt-8'>Listado de premios</h1>
+          <div className="text-sm">
+            <p>Los premios otorgados dependerán del número de participantes. En la imagen de abajo encontrarás los premios para un torneo de hasta 128 participantes. ¡Pero podrían ser más si superamos esta cifra!</p>
+            <p>¿Quién será el afortunado que se llevará la entrada a los Worlds con viaje y alojamiento pagado?</p>
+          </div>
+          <div className="relative animate-fadeInLeft mt-8">
             <Image
-              src="/images/tournaments/altered-prizes-img.jpg"
+              src="/images/tournaments/altered-prizes-image.png"
               width="1000"
               height="500"
               alt="swu-event"
               className="mx-auto hidden max-w-full drop-shadow-three dark:block dark:drop-shadow-none"
             />
+          </div>
+          <h1 className='text-xl font-bold pb-8 mt-8'>Información sobre proxies</h1>
+          <div className="text-sm">
+            <p className='mb-2'>El uso de proxies se permite únicamente para cartas únicas y siguiendo las siguientes condiciones:</p>
+            <ul className='pb-4 list-disc'>
+              <li className="mx-6 py-2">Los jugadores que usen cartas proxy deberán notificar a los jueces antes del inicio del torneo</li>
+              <li className="mx-6 py-2">La propiedad digital de las cartas únicas deberá probarse por todos los participantes a través de la aplicación oficial de Altered</li>
+              <li className="mx-6 py-2">Las cartas proxy deberán imprimirse con suficiente calidad para que permitan una lectura similar a la carta oficial</li>
+              <li className="mx-6 py-2">Las cartas proxy deberán mantenerse fuera del mazo principal y en un color de funda diferente</li>
+              <li className="mx-6 py-2">Las cartas únicas que se sustituyan por proxies deberán sustituirse en el mazo por cartas 'Convergencias de Maná' en las que se indique claramente la carta única a la que reemplazen (Ejemplo: The Hunger – WFM-096-U-15709“)</li>
+              <li className="mx-6 py-2">Las jugadores que utilicen proxies deberán informar a sus oponentes antes del inicio de cada partida y mostrar las cartas proxy boca abajo</li>
+            </ul>
+          </div>
+          <h1 className='text-xl font-bold pb-8 mt-8'>Consigue las cartas que necesites</h1>
+          <div className='text-sm mb-8'>
+            <p>Si necesitas cartas físicas para el torneo, contacta con nosotros a través de nuestro grupo de WhatsApp.
+            <a href="https://chat.whatsapp.com/JWrJAkUXPNi11hsGbgNFx0" target="_blank">
+              <FontAwesomeIcon icon={faWhatsapp} className="fas fa-instagram fa-2xl hover:text-secondary mx-2"></FontAwesomeIcon>
+            </a>
+            </p>
           </div>
         </div>
       </div>
